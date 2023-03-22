7248d3dfb5cc1e972e6e1ae96f4d832626cb7bff
theBigIsaac <65114569+theBigIsaac@users.noreply.github.com>
2023-03-22 10:25:47 +0000

    Add files via upload

--- CompSciRPG/CompSciRPG.cpp
+++ CompSciRPG/CompSciRPG.cpp
@@ -29,8 +29,7 @@ void sleep(int delay) {
     while (clock() - now < delay);
 }
 
-double randomNum(double min, double max) {
-    std::default_random_engine ranEng(time(0));
+double randomNum(double min, double max, default_random_engine ranEng) {
     std::uniform_real_distribution<double> num(min, max);
     return num(ranEng);
 }
@@ -69,10 +68,14 @@ bool mathsQuestion(string sign) {
     return success;
 }
 
-struct Weapon {
+struct Item {
+    int index;
     string name;
+    int cost;
+};
+
+struct Weapon : Item {
     double damage = 0;
-    int cost = 0;
 };
 
 list<Weapon> weapons;
@@ -85,10 +88,8 @@ Weapon GetWeapon(list<Weapon> weapons1, int x) {
     return *y;
 }
 
-struct Armour {
-    string name;
+struct Armour : Item {
     double defence = 0;
-    int cost = 0;
 };
 
 list<Armour> armours;
@@ -115,20 +116,20 @@ struct Enemy {
     double health = 40;
 };
 
-double calculateDifficulty(int score, Player mc) {
+double calculateDifficulty(int score, Player mc, default_random_engine ran) {
     double difficulty = score * 0.1;
     difficulty += mc.weapon.damage * 0.1;
     difficulty += mc.armour.defence * 0.1;
-    difficulty = randomNum(difficulty * 0.75, difficulty * 1.5);
+    difficulty = randomNum(difficulty * 0.75, difficulty * 1.5, ran);
     return difficulty;
 }
 
-Enemy createEnemy(double dificil) {
+Enemy createEnemy(double dificil, default_random_engine ran) {
     vector<string> allNames;
     vector<string> allTitles;
     string line;
     string fullName;
-    int random;   
+    int random;
 
     ifstream fileReader("names.txt");
 
@@ -174,6 +175,7 @@ Enemy createEnemy(double dificil) {
 void createWeapons() {
     Weapon temp;
     string line;
+    int lineNum = 0;
     ifstream fileReader("weapons.txt");
     string split = "\t";
 
@@ -182,6 +184,7 @@ void createWeapons() {
         string token;
         while ((position = line.find(split)) != string::npos) {
             token = line.substr(0, position);
+            temp.index = lineNum;
             temp.name = token;
             line.erase(0, position + split.length());
             position = line.find(split);
@@ -190,12 +193,14 @@ void createWeapons() {
         }
         temp.cost = stod(line);
         weapons.push_back(temp);
+        lineNum++;
     }
 }
 
 void createArmours() {
     Armour temp;
     string line;
+    int lineNum = 0;
     ifstream fileReader("armours.txt");
     string split = "\t";
 
@@ -204,6 +209,7 @@ void createArmours() {
         string token;
         while ((position = line.find(split)) != string::npos) {
             token = line.substr(0, position);
+            temp.index = lineNum;
             temp.name = token;
             line.erase(0, position + split.length());
             position = line.find(split);
@@ -212,6 +218,7 @@ void createArmours() {
         }
         temp.cost = stod(line);
         armours.push_back(temp);
+        lineNum++;
     }
 }
 
@@ -225,16 +232,106 @@ int healthCheck(Player mc, Enemy enemy) {
     else { return 2; }
 }
 
-void ShopContents(int score) {
-
+list<Weapon> WeaponShopContents(int score, default_random_engine ran) {
+    list<Weapon> contents;
+    int amount = randomNum(2, 5, ran);
+    cout << amount;
+    int location = score - 5;
+    for (int x = 0; x < amount; x++) {
+        if (ran()%100 < 70 && location < weapons.size()) {
+            contents.push_back(GetWeapon(weapons, location));
+        }
+        location++;
+    }
+    return contents;
 }
 
-Player Shop(Player mc, int score) {
-    system("cls");
+list<Armour> ArmourShopContents(int score, default_random_engine ran) {
+    list<Armour> contents;
+    int amount = randomNum(1, 3, ran);
+    int location = score - 5;
+    for (int x = 0; x < amount; x++) {
+        if (ran() % 100 < 70&& location < armours.size()) {
+            contents.push_back(GetArmour(armours, location));
+        }
+        location++;
+    }
+    return contents;
+}
+
+void ShopDisplay(Player mc, list<Weapon> displayWeapons, list<Armour> displayArmours) {
     cout << "Current Weapon: " << mc.weapon.name << "\t Damage: " << mc.weapon.damage;
     cout << "\nCurrent Armour: " << mc.armour.name << "\t Defence: " << mc.armour.defence;
-    cout << "\n\nMoney: " << mc.money;
-    sleep(2);
+    cout << "\n\nMoney: " << mc.money << "\n\n";
+    for (int i = 0; i < displayWeapons.size(); i++) {
+        Weapon temp = GetWeapon(displayWeapons, i);
+        cout << i + 1 << ": " << temp.name << "\t Damage: " << temp.damage << "\t Cost: " << temp.cost << "\n";
+    }
+    for (int i = displayWeapons.size(); i < displayArmours.size() + displayWeapons.size(); i++) {
+        Armour temp = GetArmour(displayArmours, i - displayWeapons.size());
+        cout << i + 1 << ": " << temp.name << "\t Damage: " << temp.defence << "\t Cost: " << temp.cost << "\n";
+    }
+    cout << "\nTo buy an item, type the corresponding number, or 0 to leave the shop.\n";
+}
+
+Player Shop(Player mc, int score, default_random_engine ran) {
+    system("cls");
+    list<Weapon> weaponStuff = WeaponShopContents(score, ran);
+    list<Armour> armourStuff = ArmourShopContents(score, ran);
+    ShopDisplay(mc, weaponStuff, armourStuff);
+    int choice = 1;
+    string yn;
+    while (choice != 0) {
+        cin >> choice;
+        if (choice == 0) {
+
+        }
+        else if (choice <= weaponStuff.size()) {
+            Weapon temp = GetWeapon(weaponStuff, choice - 1);
+            cout << "Do you want to purchase the " << temp.name << "? (y/n)\n";
+            cin >> yn;
+            if (yn == "y" && mc.money >= temp.cost) {
+                cout << "SOLD";
+                mc.weapon = GetWeapon(weapons, temp.index);
+                mc.money -= temp.cost;
+            }
+            else if (yn == "y" && mc.money < temp.cost) {
+                cout << "You can't afford this";
+            }
+            else if (yn == "n") {
+                cout << "Transaction cancelled";
+            }
+            else {
+                cout << "Input not understood, cancelling transaction";
+            }
+        }
+        else if (choice <= weaponStuff.size() + armourStuff.size()) {
+            Armour temp = GetArmour(armourStuff, choice - weaponStuff.size() - 1);
+            cout << "Do you want to purchase the " << temp.name << "? (y/n)\n";
+            cin >> yn;
+            if (yn == "y" && mc.money >= temp.cost) {
+                cout << "SOLD";
+                mc.armour = GetArmour(armours, temp.index);
+                mc.money -= temp.cost;
+            }
+            else if (yn == "y" && mc.money < temp.cost) {
+                cout << "You can't afford this";
+            }
+            else if (yn == "n") {
+                cout << "Transaction cancelled";
+            }
+            else {
+                cout << "Input not understood, cancelling transaction";
+            }
+
+        }
+        else {
+            cout << "Invalid number";
+        }
+        sleep(3);
+        system("cls");
+        ShopDisplay(mc, weaponStuff, armourStuff);
+    }
     return mc;
 }
 
@@ -247,7 +344,7 @@ Player Battle(Player mc, Enemy enemy) {
         cout << "You are facing " << enemy.name << "\n\n";
         cout << "Player Health: " << (int)mc.currentHP << "\n";
         cout << enemy.name << " Health: " << (int)enemy.health << "\n\n";
-        sleep(1);
+        sleep(0.5);
         bool noAttack = true;
         while (noAttack) {
             cout << "What type of attack: \nLight   Heavy   Special\n";
@@ -265,12 +362,12 @@ Player Battle(Player mc, Enemy enemy) {
                 noAttack = false;
             }
             else if (choice == "special") {
-                if (mathsQuestion("*")) { enemy.health -= (mc.weapon.damage * 2.25); }
+                if (mathsQuestion("*")) { enemy.health -= (mc.weapon.damage * 20.25); }
                 else { mc.currentHP -= (enemy.difficulty * 20.0 - mc.armour.defence); }
                 noAttack = false;
             }
         }
-        sleep(2);
+        sleep(1);
         winner = healthCheck(mc, enemy);
         switch (winner) {
         case 0:
@@ -289,16 +386,16 @@ Player Battle(Player mc, Enemy enemy) {
     return mc;
 }
 
-void game(Player mc) {
+void game(Player mc, default_random_engine ran) {
     system("cls");
-    int score = 0;
+    int score = 4;
     while (mc.currentHP > 0) {
-        double difficulty = calculateDifficulty(score, mc);
-        Enemy mainEnemy = createEnemy(difficulty);
+        double difficulty = calculateDifficulty(score, mc, ran);
+        Enemy mainEnemy = createEnemy(difficulty, ran);
         mc = Battle(mc, mainEnemy);
         score++;
-        if (score % 2 == 0) {
-            Shop(mc, score);
+        if (score % 5 == 0) {
+            mc = Shop(mc, score, ran);
         }
     }
 }
@@ -306,6 +403,7 @@ void game(Player mc) {
 int main()
 {
     srand((unsigned)time(NULL));
+    std::default_random_engine ranEng(time(0));
     createWeapons();
     createArmours();
 
@@ -319,7 +417,7 @@ int main()
     cin >> choice;
 
     if (choice == 1) {
-        game(mainCharacter);
+        game(mainCharacter, ranEng);
     }
 
     return 0;
